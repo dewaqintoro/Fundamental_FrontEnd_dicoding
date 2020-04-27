7212741dd8898ff8ee6b0a7d2da96b3454149bd2
dewaqintoro97 <dewa.qintoro@students.amikom.ac.id>
2020-04-27 19:19:59 +0700

    dewlyrics v2

---
+++ club/customElement/src/app.js
@@ -0,0 +1,6 @@
+import "./styles/style.css";
+import "./script/component/header.js";
+import "./script/component/footer.js";
+import main from "./script/view/main.js";
+ 
+document.addEventListener("DOMContentLoaded", main);
---
+++ club/customElement/src/script/component/footer.js
@@ -0,0 +1,5 @@
+function sikel() {
+    document.getElementById("sikel").innerHTML = `<p class="flow-text">dEVDew. &copy; Copyright 2020</p>`;
+}
+
+sikel()
